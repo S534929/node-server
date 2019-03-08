952176a5c1a4b2f0abf96f13fc90e372066761bf
Satish <S534929@nwmissouri.edu>
2019-03-08 16:13:10 -0600

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const http = require('http')  // use require to import the Node.js http package
 
-const hostname = '0.0.0.0'    // allows access from remote computers
+const hostname = 'localhost'    // allows access from remote computers
 const port = 3000
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+res.write(
+"<html>"+
+"<head>"+
+"<title>My first server side</title>"+
+"</head>"+
+"<body style='margin:0px;padding:0px;background:pink;color:red;'>"+
+"<div id='header' style='width:100%;height:100px;background:purple'>"+
+"<center><h1>My first server code</h1></center>"+
+"</div>"+
+"<div id='body' style='padding-top:100px;'>"+
+"<center>"+
+"<p>"+
+"This is first server app "+
+"Hello!!!"
++"</p>"+
+
+"</center>"+
+
+
+"</div>"+
+
+"</body>"+
+"</html>"
+)
+res.end()
 })
 
 server.listen(port, hostname, () => {
